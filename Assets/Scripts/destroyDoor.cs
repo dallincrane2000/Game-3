42c7d7d6971440abd0915e0115d769650717fbd2
DuckieOnQuacks <jojopaschke@gmail.com>
2022-11-21 12:27:01 -0800

    Minor Changes

--- Assets/Scripts/destroyDoor.cs
+++ Assets/Scripts/destroyDoor.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class destroyDoor : MonoBehaviour
 {
+    public GameObject door;
+    public Animator doorAnimate;
+    // Update is called once per frame
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+       //doorAnimate = this.GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         if(GameObject.FindWithTag("Enemy") == false)
         {
-            
-            Destroy(gameObject);
+            doorAnimate.SetFloat("Open", 1.0f);
+            Destroy(door.GetComponent<Collider2D>());
         }
     }
 
