169075d4d730ff7203ef956c24cd0feecb432a33
DuckieOnQuacks <jojopaschke@gmail.com>
2022-11-28 13:25:18 -0800

    Update

--- Assets/Scripts/collectSoul.cs
+++ Assets/Scripts/collectSoul.cs
@@ -6,15 +6,6 @@ public class collectSoul : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-<<<<<<< Updated upstream
-        
-    }
-
-    // Update is called once per frame
-    void OnTriggerEnter2D(Collider2D other)
-    {
-=======
->>>>>>> Stashed changes
         if(other.gameObject.tag == "Player")
         {
             Destroy(gameObject);
